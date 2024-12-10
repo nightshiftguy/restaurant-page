c5293a083586647bfebb56e0d3188c5c2603cbd3
Jan <jas.gol@poczta.onet.pl>
2024-12-11 00:04:43 +0100

    move homepage content to separate js module

---
+++ src/homepage.js
@@ -0,0 +1,19 @@
+import restaurantImg from "./img/restaurant.jpg";
+
+export default function pageLoad(){
+    const container = document.querySelector("#content");
+
+    const mainImage = document.createElement("img");
+    mainImage.setAttribute("id", "main-image");
+    mainImage.src = restaurantImg;
+
+    const header = document.createElement("h1");
+    header.textContent = "Super restaurant";
+
+    const shortText = document.createElement("p");
+    shortText.textContent = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti reprehenderit voluptas, natus, voluptatibus eius nam at vero voluptate maxime beatae est! Sunt pariatur doloribus, maxime distinctio iste eum nobis rem?";
+    
+    container.appendChild(mainImage);
+    container.appendChild(header);
+    container.appendChild(shortText);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import loadHomepage from "./homepage.js";
+
+loadHomepage();
